9e0abec56e920b8a18a52e872ffc49c32b75f0bf
KunalKurve <osheenmanasi@gmail.com>
2026-05-14 01:47:52 +0530

    adding the create and delete group responsedtos

---
+++ src/main/java/com/example/splitwise/dtos/responses/CreateGroupResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.splitwise.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CreateGroupResponseDto {
+
+    private ResponseStatus responseStatus;
+    private int groupId;
+    private int adminId;
+}
---
+++ src/main/java/com/example/splitwise/dtos/responses/DeleteGroupResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.splitwise.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteGroupResponseDto {
+
+    private ResponseStatus responseStatus;
+
+}
