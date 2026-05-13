ceb6bf3a21512d9044dba4fd277a1815fe1875ae
KunalKurve <osheenmanasi@gmail.com>
2026-05-14 01:29:19 +0530

    Extracted Common code for aggregating expenses from list of expenses to map of user and amount

---
+++ src/main/java/com/example/splitwise/utils/ExpenseUtils.java
@@ -0,0 +1,32 @@
+package com.example.splitwise.utils;
+
+import com.example.splitwise.models.Expense;
+import com.example.splitwise.models.ExpenseUser;
+import com.example.splitwise.models.User;
+import com.example.splitwise.models.enums.ExpenseType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExpenseUtils {
+
+    public static Map<User, Double> aggregateExpenses(List<Expense> expenses){
+
+        Map<User, Double> userExpenses = new HashMap<>();
+
+        for(Expense expense : expenses) {
+            for(ExpenseUser expenseUser : expense.getUserExpenses()){
+
+                Double userAmount = expenseUser.getExpenseType() == ExpenseType.PAID ?
+                        expenseUser.getAmount() : -1 * expenseUser.getAmount();
+
+                userExpenses.put(expenseUser.getUser(),
+                        userExpenses.getOrDefault(
+                                expenseUser.getUser(), 0d) + userAmount);
+            }
+        }
+
+        return userExpenses;
+    }
+}
