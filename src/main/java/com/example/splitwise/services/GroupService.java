b6e0a12cf06b8b83a4d2271deb61c5e546228942
KunalKurve <osheenmanasi@gmail.com>
2026-05-14 01:48:09 +0530

    adding GroupService

--- src/main/java/com/example/splitwise/services/GroupService.java
+++ src/main/java/com/example/splitwise/services/GroupService.java
@@ -1,4 +1,22 @@
 package com.example.splitwise.services;
 
+import com.example.splitwise.models.Group;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class GroupService {
+    public void deleteGroup(int groupId, int adminId) {
+    }
+
+    public Group createGroup(String groupName, String description, int userId) {
+
+        Group group = new Group();
+        group.setCreatedAt(new Date());
+        group.setGroupName(groupName);
+        group.setDescription(description);
+
+        return group;
+    }
 }
