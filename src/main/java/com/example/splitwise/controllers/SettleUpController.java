64d25cf206a7e205ab2c7b027930faff60a0c395
KunalKurve <osheenmanasi@gmail.com>
2026-05-14 01:33:19 +0530

    Updated the Controller and Service files to full functionality

--- src/main/java/com/example/splitwise/controllers/SettleUpController.java
+++ src/main/java/com/example/splitwise/controllers/SettleUpController.java
@@ -1,4 +1,56 @@
 package com.example.splitwise.controllers;
 
+import com.example.splitwise.dtos.requests.SettleGroupRequestDto;
+import com.example.splitwise.dtos.requests.SettleUserRequestDto;
+import com.example.splitwise.dtos.responses.ResponseStatus;
+import com.example.splitwise.dtos.responses.SettleGroupResponseDto;
+import com.example.splitwise.dtos.responses.SettleUserResponseDto;
+import com.example.splitwise.dtos.responses.Transaction;
+import com.example.splitwise.services.SettleUpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
 public class SettleUpController {
+
+    private SettleUpService settleUpService;
+
+    @Autowired
+    public SettleUpController(SettleUpService settleUpService){
+        this.settleUpService = settleUpService;
+    }
+
+    public SettleGroupResponseDto settleUpGroup(SettleGroupRequestDto requestDto){
+        SettleGroupResponseDto responseDto = new SettleGroupResponseDto();
+        try{
+            List<Transaction> transactions = settleUpService.settleGroup(
+                    requestDto.getGroupId()
+            );
+            responseDto.setTransactions(transactions);
+            responseDto.setResponseStatus(ResponseStatus.SUCCESS);
+            responseDto.setMessage("Group has been Settled!");
+        } catch (Exception e) {
+            responseDto.setResponseStatus(ResponseStatus.FAILED);
+            responseDto.setMessage("Group couldn't be Settled!");
+        }
+        return responseDto;
+    }
+
+    public SettleUserResponseDto settleUpUser(SettleUserRequestDto requestDto){
+        SettleUserResponseDto responseDto = new SettleUserResponseDto();
+        try{
+            List<Transaction> transactions = settleUpService.settleUser(
+                    requestDto.getUserId()
+            );
+            responseDto.setTransactions(transactions);
+            responseDto.setResponseStatus(ResponseStatus.SUCCESS);
+            responseDto.setMessage("User Expenses have been Settled!");
+        } catch (Exception e) {
+            responseDto.setResponseStatus(ResponseStatus.FAILED);
+            responseDto.setMessage("User Expenses couldn't be Settled!");
+        }
+        return responseDto;
+    }
 }
--- src/main/java/com/example/splitwise/services/SettleUpService.java
+++ src/main/java/com/example/splitwise/services/SettleUpService.java
@@ -1,4 +1,62 @@
 package com.example.splitwise.services;
 
+import com.example.splitwise.dtos.responses.Transaction;
+import com.example.splitwise.exceptions.InvalidGroupException;
+import com.example.splitwise.exceptions.InvalidUserException;
+import com.example.splitwise.models.Expense;
+import com.example.splitwise.models.Group;
+import com.example.splitwise.models.User;
+import com.example.splitwise.repositories.ExpenseRepository;
+import com.example.splitwise.repositories.ExpenseUserRepository;
+import com.example.splitwise.repositories.GroupRepository;
+import com.example.splitwise.repositories.UserRepository;
+import com.example.splitwise.strategies.SettleUpStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SettleUpService {
+
+    private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+    private final ExpenseRepository expenseRepository;
+    private final ExpenseUserRepository expenseUserRepository;
+    private final SettleUpStrategy settleUpStrategy;
+
+    @Autowired
+    public SettleUpService(
+            UserRepository userRepository,
+            GroupRepository groupRepository,
+            ExpenseRepository expenseRepository,
+            ExpenseUserRepository expenseUserRepository,
+            SettleUpStrategy settleUpStrategy
+    ){
+        this.userRepository = userRepository;
+        this.groupRepository =  groupRepository;
+        this.expenseRepository = expenseRepository;
+        this.expenseUserRepository = expenseUserRepository;
+        this.settleUpStrategy = settleUpStrategy;
+    }
+
+    public List<Transaction> settleGroup(int groupId) throws InvalidGroupException {
+
+        Group group = groupRepository.findById(groupId)
+                .orElseThrow(()-> new InvalidGroupException("Group not found"));
+
+        List<Expense> expenses = expenseRepository.findAllByGroup_Id(group.getId());
+
+        return settleUpStrategy.settleUp(expenses);
+    }
+
+    public List<Transaction> settleUser(int userId) throws InvalidUserException {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new InvalidUserException("User not found"));
+
+        List<Expense> expenses = expenseRepository.findNonGroupExpensesForUser(user.getId());
+
+        return settleUpStrategy.settleUp(expenses);
+    }
 }
