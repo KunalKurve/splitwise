96cf5f1c62af8df1cbbfe0a3e3eef2d18cbd88af
KunalKurve <osheenmanasi@gmail.com>
2026-05-14 01:27:19 +0530

    Made changes in Repository to support SettleUpService

--- src/main/java/com/example/splitwise/repositories/ExpenseRepository.java
+++ src/main/java/com/example/splitwise/repositories/ExpenseRepository.java
@@ -2,8 +2,16 @@ package com.example.splitwise.repositories;
 
 import com.example.splitwise.models.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
+    List<Expense> findAllByGroup_Id(int groupId);
+
+    @Query("select e from Expense e where e.group is null and e.user.id = :userId")
+    List<Expense> findNonGroupExpensesForUser(@Param("userId") int userId);
 }
