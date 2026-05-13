838529496be04972fa3fcf0aa38da74ffbda4497
KunalKurve <osheenmanasi@gmail.com>
2026-05-14 01:30:57 +0530

    Added Getters and Setters

--- src/main/java/com/example/splitwise/dtos/requests/SettleGroupRequestDto.java
+++ src/main/java/com/example/splitwise/dtos/requests/SettleGroupRequestDto.java
@@ -1,4 +1,11 @@
 package com.example.splitwise.dtos.requests;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SettleGroupRequestDto {
+
+    private int groupId;
 }
--- src/main/java/com/example/splitwise/dtos/requests/SettleUserRequestDto.java
+++ src/main/java/com/example/splitwise/dtos/requests/SettleUserRequestDto.java
@@ -1,6 +1,11 @@
 package com.example.splitwise.dtos.requests;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class SettleUserRequestDto {
 
-
+    private int userId;
 }
