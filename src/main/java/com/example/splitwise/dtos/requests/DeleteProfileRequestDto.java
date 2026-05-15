141ef160266a85242bea7131bed47accaa3c9967
KunalKurve <osheenmanasi@gmail.com>
2026-05-15 18:45:09 +0530

    delete profile request dto

---
+++ src/main/java/com/example/splitwise/dtos/requests/DeleteProfileRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.splitwise.dtos.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteProfileRequestDto {
+
+    private int userId;
+}
