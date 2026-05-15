9287194685e63436c9a25addb8e387b0f6a0db19
KunalKurve <osheenmanasi@gmail.com>
2026-05-15 18:44:35 +0530

    delete group command creation file

---
+++ src/main/java/com/example/splitwise/commands/DeleteGroup.java
@@ -0,0 +1,45 @@
+package com.example.splitwise.commands;
+
+import com.example.splitwise.controllers.GroupController;
+import com.example.splitwise.dtos.requests.DeleteGroupRequestDto;
+import com.example.splitwise.dtos.responses.DeleteGroupResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteGroup implements Command{
+
+    private final GroupController groupController;
+    private final String COMMAND_NAME = "DeleteGroup";
+
+    @Autowired
+    public DeleteGroup(GroupController groupController){
+        this.groupController = groupController;
+    }
+
+    @Override
+    public boolean matches(String input) {
+        String[] commandFragments = input.split(" ");
+        if(commandFragments[0].equalsIgnoreCase(COMMAND_NAME)){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void execute(String input) {
+        String[] commandFragments = input.split(" ");
+        try{
+            int groupId = Integer.parseInt(commandFragments[1]);
+            int adminId = Integer.parseInt(commandFragments[2]);
+            DeleteGroupRequestDto requestDto = new DeleteGroupRequestDto();
+            requestDto.setGroupId(groupId);
+            requestDto.setAdminId(adminId);
+
+            DeleteGroupResponseDto responseDto = groupController.deleteGroup(requestDto);
+            System.out.println(responseDto.getResponseStatus());
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Something went Wrong !");
+        }
+    }
+}
