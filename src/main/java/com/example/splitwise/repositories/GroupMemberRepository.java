833fa18c6dc3ad20523656d1f1616d183232999d
KunalKurve <osheenmanasi@gmail.com>
2026-05-15 18:49:27 +0530

    added group member repository

---
+++ src/main/java/com/example/splitwise/repositories/GroupMemberRepository.java
@@ -0,0 +1,17 @@
+package com.example.splitwise.repositories;
+
+import com.example.splitwise.models.GroupMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GroupMemberRepository extends JpaRepository<GroupMember, Integer> {
+    void deleteByGroupId(int groupId);
+
+    Optional<GroupMember> findByGroupIdAndUserId(int groupId, int memberToBeAddedUserId);
+
+    List<GroupMember> findAllByGroupId(int groupId);
+}
