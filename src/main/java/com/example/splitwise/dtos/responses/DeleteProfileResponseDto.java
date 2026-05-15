30e46883c87e1bedf407aeee9e9041e91e9ea345
KunalKurve <osheenmanasi@gmail.com>
2026-05-15 18:45:17 +0530

    delete profile response dto

---
+++ src/main/java/com/example/splitwise/dtos/responses/DeleteProfileResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.splitwise.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteProfileResponseDto {
+
+    private ResponseStatus responseStatus;
+}
