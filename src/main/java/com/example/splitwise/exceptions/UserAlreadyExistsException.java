e3b727bf85ae07b8f5c1519ee8bf59b60e2dc60a
KunalKurve <osheenmanasi@gmail.com>
2026-05-15 18:41:36 +0530

    renamed Exception

---
+++ src/main/java/com/example/splitwise/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.splitwise.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
