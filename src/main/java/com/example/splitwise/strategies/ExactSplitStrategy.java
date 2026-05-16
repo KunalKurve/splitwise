62bf4914b13627a64713e741588047a118fe01cd
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-16 18:37:41 +0530

    Support for Exact Split Strategy

---
+++ src/main/java/com/example/splitwise/strategies/ExactSplitStrategy.java
@@ -0,0 +1,36 @@
+package com.example.splitwise.strategies;
+
+import com.example.splitwise.dtos.requests.SplitRequestDto;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ExactSplitStrategy implements SplitStrategy {
+
+    @Override
+    public Map<Integer, Double> calculateSplits(
+            Double totalAmount,
+            List<SplitRequestDto> splitRequestDtos
+    ) {
+
+        Map<Integer, Double> result = new HashMap<>();
+
+        double sum = 0;
+
+        for(SplitRequestDto dto : splitRequestDtos) {
+
+            result.put(dto.getUserId(), dto.getAmount());
+
+            sum += dto.getAmount();
+        }
+
+        if(sum != totalAmount) {
+            throw new RuntimeException("Invalid exact split amounts");
+        }
+
+        return result;
+    }
+}
