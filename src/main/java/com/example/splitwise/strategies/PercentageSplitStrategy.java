7d7cd6123dab3c2e287a253303c9936f10c8f4f1
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-16 18:37:51 +0530

    Support for Percentage Split Strategy

---
+++ src/main/java/com/example/splitwise/strategies/PercentageSplitStrategy.java
@@ -0,0 +1,39 @@
+package com.example.splitwise.strategies;
+
+import com.example.splitwise.dtos.requests.SplitRequestDto;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class PercentageSplitStrategy implements SplitStrategy {
+
+    @Override
+    public Map<Integer, Double> calculateSplits(
+            Double totalAmount,
+            List<SplitRequestDto> splitRequestDtos
+    ) {
+
+        Map<Integer, Double> result = new HashMap<>();
+
+        double totalPercent = 0;
+
+        for(SplitRequestDto dto : splitRequestDtos) {
+
+            totalPercent += dto.getPercentage();
+
+            double splitAmount =
+                    (dto.getPercentage() * totalAmount) / 100;
+
+            result.put(dto.getUserId(), splitAmount);
+        }
+
+        if(totalPercent != 100) {
+            throw new RuntimeException("Percent total must be 100");
+        }
+
+        return result;
+    }
+}
