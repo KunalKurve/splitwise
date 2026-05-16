9fafbed7b3dd9effb47b62f6b33e5412beb27bb8
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-16 21:22:19 +0530

    added toString method

--- src/main/java/com/example/splitwise/dtos/responses/MemberDto.java
+++ src/main/java/com/example/splitwise/dtos/responses/MemberDto.java
@@ -2,9 +2,11 @@ package com.example.splitwise.dtos.responses;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class MemberDto {
 
     private int id;
