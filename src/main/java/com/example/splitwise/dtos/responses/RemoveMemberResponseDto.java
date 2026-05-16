a2e9c67fbb744ce3bc963a9bd400b33091fd94a9
KunalKurve <osheenmanasi@gmail.com>
2026-05-16 17:58:52 +0530

    adding request and response dto for remove member command

---
+++ src/main/java/com/example/splitwise/dtos/responses/RemoveMemberResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.splitwise.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RemoveMemberResponseDto {
+
+    private ResponseStatus responseStatus;
+}
