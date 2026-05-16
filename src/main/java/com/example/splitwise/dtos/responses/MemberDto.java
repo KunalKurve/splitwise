04dc57e7d563e4744136b642dd91a40ecbd7d322
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-16 20:07:43 +0530

    added new member dto to support fetch all members dto

---
+++ src/main/java/com/example/splitwise/dtos/responses/MemberDto.java
@@ -0,0 +1,13 @@
+package com.example.splitwise.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+
+    private int id;
+    private String name;
+    private String phone;
+}
