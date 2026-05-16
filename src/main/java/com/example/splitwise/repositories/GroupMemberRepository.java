adb11c09907aefcd85c7a0f96ef6f0014818d97e
KunalKurve <165116100+KunalKurve@users.noreply.github.com>
2026-05-16 21:22:45 +0530

    added query to fetch groupmember list eagerly

--- src/main/java/com/example/splitwise/repositories/GroupMemberRepository.java
+++ src/main/java/com/example/splitwise/repositories/GroupMemberRepository.java
@@ -2,6 +2,7 @@ package com.example.splitwise.repositories;
 
 import com.example.splitwise.models.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +14,11 @@ public interface GroupMemberRepository extends JpaRepository<GroupMember, Intege
     void deleteByGroupId(int groupId);
 
     Optional<GroupMember> findByGroupIdAndMemberId(int groupId, int memberToBeAddedUserId);
-
+    @Query("""
+    SELECT gm
+    FROM GroupMember gm
+    JOIN FETCH gm.member
+    WHERE gm.group.id=:groupId
+    """)
     List<GroupMember> findAllByGroupId(int groupId);
 }
